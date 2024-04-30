9e06fdb9d57f5965aa628c527802f7ed7517c419
Boi Octave <boiOctave@gmail.com>
2024-04-30 17:33:51 +0100

    nav

--- app/(home)/components/NavBar.tsx
+++ app/(home)/components/NavBar.tsx
@@ -27,9 +27,7 @@ export default function NavBar({className}:{className?:string}) {
         <h1 className='text-2x1 font-bold underline underline-offset-8 decoration-green-500 -rotate-2'> TechWrizz   💻 </h1>
     <div className="flex items-center gap-5">
         {socials.map((social, index)=>{
-
             const Icon = social.Icon
-
             return <Link href={social.link} key={index} aria-label={social.label}>
                 <Icon className='w-5 h-5 hover:scale-125 transition-all'/>
             </Link>
